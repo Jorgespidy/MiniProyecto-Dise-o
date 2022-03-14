17086498a859b637ca581e918abf8c51d0a060a4
Jorgespidy <78282007+Jorgespidy@users.noreply.github.com>
2022-03-13 22:05:30 -0600

    Comunicación entre MPU y Tiva. Creación de librerías I1C & UART

---
+++ Control_PID_balancin/main.c
@@ -0,0 +1,41 @@
+
+
+/**
+ * main.c
+ */
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include "sensorlib/i2cm_drv.h"
+#include "sensorlib/hw_mpu6050.h"
+#include "sensorlib/mpu6050.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_types.h"
+#include "inc/hw_i2c.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/debug.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/i2c.h"
+#include "driverlib/sysctl.h"
+
+#include <math.h>
+
+//#include "uart.h"
+#include "utils/uartstdio.h"
+#include "driverlib/uart.h"
+
+
+int main(void)
+{
+	return 0;
+}
