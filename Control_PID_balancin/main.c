b9605ef2050f2e94e1627d031a8834ce65988b5f
Jorgespidy <78282007+Jorgespidy@users.noreply.github.com>
2022-03-18 02:28:05 -0600

    Lectura de ejes X & Y del giroscopio y acelerometro

--- Control_PID_balancin/main.c
+++ Control_PID_balancin/main.c
@@ -34,8 +34,191 @@
 #include "utils/uartstdio.h"
 #include "driverlib/uart.h"
 
+//
+// A boolean that is set when a MPU6050 command has completed.
+//
+volatile bool g_bMPU6050Done;
+
+//
+// I2C master instance
+//
+tI2CMInstance g_sI2CMSimpleInst;
+
+//
+//Device frequency
+//
+int clockFreq;
+
+
+void InitI2C0(void)
+{
+    //enable del modulo I2C 0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
+
+    //reset
+    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C0);
+
+    //enable de los puertos del I2C 0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+    // Configuracion de puertos B2 and B3.
+    GPIOPinConfigure(GPIO_PB2_I2C0SCL);
+    GPIOPinConfigure(GPIO_PB3_I2C0SDA);
+
+    // Asigna comunicacion I2C a los pines.
+    GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, GPIO_PIN_2);
+    GPIOPinTypeI2C(GPIO_PORTB_BASE, GPIO_PIN_3);
+
+    // Activa e inicializa modulo I2C.
+    // Se usa el system clock
+    // Transmision de datos a 400kbps.
+    I2CMasterInitExpClk(I2C0_BASE, SysCtlClockGet(), true);
+
+    // Limpieza de FIFOs
+    HWREG(I2C0_BASE + I2C_O_FIFOCTL) = 80008000; // que es toda esta shit?
+
+    // Inicia I2C master driver
+    I2CMInit(&g_sI2CMSimpleInst, I2C0_BASE, INT_I2C0, 0xff, 0xff, SysCtlClockGet());
+
+}
+
+
+void ConfigureUART(void) {
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); // pines pertenecientes al UART0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0); // UART0 comunica con la computadora
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    UARTStdioConfig(0, 115200, 16000000); // UART0 a 115200 baudrate, frecuencia de 16MHz
+}
+
+void delayMS(int ms) {
+    ROM_SysCtlDelay( (ROM_SysCtlClockGet()/(3*1000))*ms ) ;  // more accurate
+    //SysCtlDelay( (SysCtlClockGet()/(3*1000))*ms ) ;  // less accurate
+}
+
+//
+// The function that is provided by this example as a callback when MPU6050
+// transactions have completed.
+//
+void MPU6050Callback(void *pvCallbackData, uint_fast8_t ui8Status)
+{
+    //
+    // See if an error occurred.
+    //
+    if (ui8Status != I2CM_STATUS_SUCCESS)
+    {
+        //
+        // An error occurred, so handle it here if required.
+        //
+    }
+    //
+    // Indicate that the MPU6050 transaction has completed.
+    //
+    g_bMPU6050Done = true;
+}
+
+//
+// The interrupt handler for the I2C module.
+//
+void I2CMSimpleIntHandler(void)
+{
+    //
+    // Call the I2C master driver interrupt handler.
+    //
+    I2CMIntHandler(&g_sI2CMSimpleInst);
+}
+
+
+
+//
+// Configuracion del muestreo del MPU.
+//
+void MPU6050Example(void)
+{
+    float fAccel[3], fGyro[3];
+    tMPU6050 sMPU6050;
+    float x = 0, y = 0, z = 0;
+
+    //
+    // Initialize the MPU6050. This code assumes that the I2C master instance
+    // has already been initialized.
+    //
+    g_bMPU6050Done = false;
+    MPU6050Init(&sMPU6050, &g_sI2CMSimpleInst, 0x68, MPU6050Callback, &sMPU6050); // 0x68 slave adress
+    while (!g_bMPU6050Done)
+    {
+    }
+
+    //
+    // Configura el rango del acelerometro del MPU a +/- 2 g.
+    //
+    g_bMPU6050Done = false;
+    MPU6050ReadModifyWrite(&sMPU6050, MPU6050_O_ACCEL_CONFIG, ~MPU6050_ACCEL_CONFIG_AFS_SEL_M,
+        MPU6050_ACCEL_CONFIG_AFS_SEL_2G, MPU6050Callback, &sMPU6050);
+    while (!g_bMPU6050Done)
+    {
+    }
+
+
+    g_bMPU6050Done = false;
+    MPU6050ReadModifyWrite(&sMPU6050, MPU6050_O_PWR_MGMT_1, 0x00, 0b00000010 & MPU6050_PWR_MGMT_1_DEVICE_RESET, MPU6050Callback, &sMPU6050);
+    while (!g_bMPU6050Done)
+    {
+    }
+
+    g_bMPU6050Done = false;
+    MPU6050ReadModifyWrite(&sMPU6050, MPU6050_O_PWR_MGMT_2, 0x00, 0x00, MPU6050Callback, &sMPU6050);
+    while (!g_bMPU6050Done)
+    {
+    }
+
+
+    while (1)
+    {
+        //
+        // Request another reading from the MPU6050.
+        //
+        g_bMPU6050Done = false;
+        MPU6050DataRead(&sMPU6050, MPU6050Callback, &sMPU6050);
+        while (!g_bMPU6050Done)
+        {
+        }
+        //
+        // Get the new accelerometer and gyroscope readings.
+        //
+        MPU6050DataAccelGetFloat(&sMPU6050, &fAccel[0], &fAccel[1], &fAccel[2]);
+        MPU6050DataGyroGetFloat(&sMPU6050, &fGyro[0], &fGyro[1], &fGyro[2]);
+        //
+        // Do something with the new accelerometer and gyroscope readings.
+        //
+
+        x = fGyro[0];
+        y = fGyro[1];
+        z = fGyro[2];
+
+        x = (atan2(fAccel[0], sqrt (fAccel[1] * fAccel[1] + fAccel[2] * fAccel[2]))*180.0)/3.14;
+
+        y = (atan2(fAccel[1], sqrt (fAccel[0] * fAccel[0] + fAccel[2] * fAccel[2]))*180.0)/3.14;
+
+
+        UARTprintf("Ang. X: %d | Ang. Y: %d | Ang. Z: %d\n", (int)x, (int)y, (int)z);
+
+
+        delayMS(100);
+    }
+}
+
+
 
 int main(void)
 {
-	return 0;
+    //clockFreq = SysCtlClockFreqSet(SYSCTL_OSC_INT | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480, 16000000);
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_PLL | SYSCTL_OSC_INT | SYSCTL_XTAL_16MHZ);
+
+    InitI2C0();
+    ConfigureUART();
+    MPU6050Example();
+    return(0);
 }
